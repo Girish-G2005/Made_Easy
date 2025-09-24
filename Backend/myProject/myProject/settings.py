8726f399a62b39a331f25c7fc014a8975b263726
Girish-G2005 <girish.co026@gmail.com>
2025-09-24 23:23:06 +0530

    Correction

--- Backend/myProject/myProject/settings.py
+++ Backend/myProject/myProject/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-yiim%c*gub^5buzi^nk4y6p(b=+j3d5*h!w8*-yt_=8)bgvkiu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["made-easy-8nr7.onrender.com", "https://made-easy-8nr7.onrender.com"]
+ALLOWED_HOSTS = ["made-easy-8nr7.onrender.com"]
 
 
 # Application definition
